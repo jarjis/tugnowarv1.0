08484e0e0d23df5c994af24bc20baaf2733acda5
jarjis <jarjis@gmail.com>
2013-01-24 21:21:22 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tugnowarv10::Application.config.secret_token = '0a3e2307ef7c8509070a204c5f13844a0419b330c4ad6cf23f7ad12721f3e2beb5dde07ac10094ef43b4b9d3ee2238ffe86ae58cd9f0a9d0ffcabb5b13aa6627'
